59b5bad46bb2fedbf49ba077f78ca6cefd1979ad
LyciaKremer <lyciakremer@gmail.com>
2023-09-23 19:19:21 -0300

    meta update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,7 +43,15 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
-
+          <meta property="og:title" content="Feirão Benny's" />
+          <meta
+            property="og:description"
+            content="Vem dar uma olhada nos carros incríveis que estarão no feirão!"
+          />
+          <meta
+            property="og:image"
+            content="https://cdn.discordapp.com/attachments/1115975474074619915/1143645854360666132/Instagram_Cidade.png"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
